26638cdcaa5e936b22ddd6edf670c625e61ca062
Brian Routhaues Boeg Stjernholm <au170668@uni.au.dk>
2020-03-03 11:46:26 +0100

    solution added

---
+++ StockTradingSystem_Observer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTradingSystem_Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
